e7d26bc8e7342cbd1b4245c3e9a85b7962f349f8
Brandon Chiarito <bchiarito@gmail.com>
2022-06-17 10:16:02 -0500

    finished decay mode categorizing

--- PhysicsTools/NanoAODTools/python/postprocessing/modules/customGenPartModule.py
+++ PhysicsTools/NanoAODTools/python/postprocessing/modules/customGenPartModule.py
@@ -4,6 +4,8 @@ import ROOT
 ROOT.PyConfig.IgnoreCommandLineOptions = True
 import math
 
+from collections import Counter
+
 class customGenPartModule(Module):
     def __init__(self, signal):
         self.signal = signal
@@ -33,13 +35,76 @@ class customGenPartModule(Module):
         self.out.branch("GenOmega_status", "F", lenVar="nGenOmega")
         self.out.branch("GenOmega_index", "F", lenVar="nGenOmega")
         self.out.branch("GenOmega_decaymode", "F", lenVar="nGenOmega")
+        self.out.branch("GenOmega_prongs", "F", lenVar="nGenOmega")
 
     def endFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
         pass
 
-    def get_decaymode(self, genparticle):
-        # FIXME
-        return 0
+    def get_decaymode(self, index, genparticles):
+        #print "finding daughters for", genparticles[index].pdgId
+        daughters = []
+        granddaughters = []
+        for i, genparticle in enumerate(genparticles):
+          if genparticle.genPartIdxMother == index: daughters.append(genparticle.pdgId)
+          if genparticle.pdgId == 221 and genparticle.genPartIdxMother == index:
+            for g in genparticles:
+              if g.genPartIdxMother == i: granddaughters.append(g.pdgId)
+        #for d in daughters:
+        #  print "  ", d
+        #for d in granddaughters:
+        #  print "    ", d
+
+        # eta modes
+        if Counter(daughters) == Counter([22, 22]): return 1
+        elif Counter(daughters) == Counter([111, 111, 111]): return 2
+        elif Counter(daughters) == Counter([211, -211, 111]): return 3
+        elif Counter(daughters) == Counter([211, -211, 22]): return 4
+
+        # etaprime modes
+        elif (Counter(daughters) == Counter([111, 111, 221])
+             and Counter(granddaughters) == Counter([22, 22])): return 5
+        elif (Counter(daughters) == Counter([111, 111, 221])
+             and Counter(granddaughters) == Counter([111, 111, 111])): return 6
+        elif (Counter(daughters) == Counter([22, 22])): return 7 # can never return
+        elif (Counter(daughters) == Counter([211, -211, 221])
+             and Counter(granddaughters) == Counter([22, 22])): return 8
+        elif (Counter(daughters) == Counter([211, -211, 221])
+             and Counter(granddaughters) == Counter([111, 111, 111])): return 9
+        elif (Counter(daughters) == Counter([111, 111, 221])
+             and Counter(granddaughters) == Counter([211, -211, 111])): return 10
+        elif (Counter(daughters) == Counter([111, 111, 221])
+             and Counter(granddaughters) == Counter([211, -211, 22])): return 11
+        elif (Counter(daughters) == Counter([22, 113])
+             and Counter(granddaughters) == Counter([211, -211])): return 12
+        elif (Counter(daughters) == Counter([22, 223])
+             and Counter(granddaughters) == Counter([211, -211, 111])): return 13
+        elif (Counter(daughters) == Counter([211, -211, 221])
+             and Counter(granddaughters) == Counter([211, -211, 111])): return 14
+        elif (Counter(daughters) == Counter([211, -211, 221])
+             and Counter(granddaughters) == Counter([211, -211, 22])): return 15
+
+        # cannot determine
+        else: return 0
+
+    def get_prongs(self, decaymode):
+        return {
+          0: -1,
+          1: 0,
+          2: 0,
+          3: 2,
+          4: 2,
+          5: 0,
+          6: 0,
+          7: 0,
+          8: 2,
+          9: 2,
+          10: 2,
+          11: 2,
+          12: 2,
+          13: 2,
+          14: 4,
+          15: 4
+        }.get(decaymode, -1)
 
     def analyze(self, event):
         """process event, return True (go to next module) or False (fail, go to next event)"""
@@ -60,6 +125,7 @@ class customGenPartModule(Module):
         GenOmega_status = []
         GenOmega_index = []
         GenOmega_decaymode = []
+        GenOmega_prongs = []
 
         genis = []
         for i in range(len(genparticles)):
@@ -67,7 +133,6 @@ class customGenPartModule(Module):
           if genparticle.pdgId == 54:
             genis.append(i)
 
-        print genis
         last_part_index = -1
         if len(genis) == 1: last_part_index = genis[0]
         else:
@@ -97,8 +162,9 @@ class customGenPartModule(Module):
           GenOmega_mass.append(genparticles[t].mass)
           GenOmega_status.append(genparticles[t].status)
           GenOmega_index.append(t)
-          decaymode = self.get_decaymode(genparticles[t])
+          decaymode = self.get_decaymode(t, genparticles)
           GenOmega_decaymode.append(decaymode)
+          GenOmega_prongs.append(self.get_prongs(decaymode))
 
         if self.signal == 'res':  
           for i in range(len(genparticles)):
@@ -110,8 +176,9 @@ class customGenPartModule(Module):
               GenOmega_mass.append(genparticle.mass)
               GenOmega_status.append(genparticle.status)
               GenOmega_index.append(i)
-              decaymode = self.get_decaymode(genparticle)
+              decaymode = self.get_decaymode(i, genparticles)
               GenOmega_decaymode.append(decaymode)
+              GenOmega_prongs.append(self.get_prongs(decaymode))
 
         # fill branches
         if self.signal == 'res':
@@ -132,6 +199,7 @@ class customGenPartModule(Module):
         self.out.fillBranch("GenOmega_status", GenOmega_status)
         self.out.fillBranch("GenOmega_index", GenOmega_index)
         self.out.fillBranch("GenOmega_decaymode", GenOmega_decaymode)
+        self.out.fillBranch("GenOmega_prongs", GenOmega_prongs)
         return True
 
 genpartConstr_res = lambda: customGenPartModule(signal='res') # resonant, Phi > omega omega
