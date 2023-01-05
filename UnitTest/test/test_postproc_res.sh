06e3aa0838d602e671da268fc94c570ea50450e4
Brandon Chiarito <bchiarito@gmail.com>
2023-01-05 17:48:15 -0600

    different unit test

--- UnitTest/test/test_postproc_res.sh
+++ UnitTest/test/test_postproc_res.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 printf "### default: twoprong, photon, selector\n\n"
-python ../../PhysicsTools/NanoAODTools/scripts/nano_postproc.py . NanoAOD.root -I PhysicsTools.NanoAODTools.postprocessing.modules.main genpartConstr_res,filtersConstr_default,twoprongConstr_addLoose,photonConstr_default,recoPhiConstr_HPID,recoPhiConstr_cutBased,selectionConstr_default --bo ../../PhysicsTools/NanoAODTools/test/dropPF.txt
+python ../../PhysicsTools/NanoAODTools/scripts/nano_postproc.py . NanoAOD.root -I PhysicsTools.NanoAODTools.postprocessing.modules.main genpartConstr_res,filtersConstr_default,twoprongConstr_optionalTrack_addLoose,photonConstr_default,recoPhiConstr_HPID,recoPhiConstr_cutBased,selectionConstr_muonelectronphoton --bo ../../PhysicsTools/NanoAODTools/test/dropPF.txt
 printf "### finish: copy tree, histogram\n\n"
 python ../../PhysicsTools/NanoAODTools/test/copy_tree.py NanoAOD_Skim.root
 python plot_test_twoprong.py NANOAOD_TwoProng.root
